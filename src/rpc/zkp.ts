c0d38ad97ce91ede067b2e535ab5f08da9dfb7fd
ququzone <xueping.yang@gmail.com>
2023-09-08 00:32:19 +0800

    complete verify proof

--- src/rpc/zkp.ts
+++ src/rpc/zkp.ts
@@ -8,19 +8,24 @@ const provider = new ethers.providers.JsonRpcProvider(
 const main = async () => {
   const signer = new ethers.Wallet(process.env.PRIVATE_KEY || '', provider);
 
+  let contract = '0x5b3c80eaD459C3De05d1BB6F99D0B9Efaab458c2';
+  if (!process.env.SKIP_DEPLOY) {
+    console.log('deploy contract ...');
+    const tx = await signer.sendTransaction({
+      data: data.contract,
+    });
+
+    const receipt = await tx.wait();
+    contract = receipt.contractAddress;
+    console.log(`deployed contract address: ${contract}`);
+  }
+
+  console.log('verify proof ...');
   const tx = await signer.sendTransaction({
-    data: data.contract,
-  });
-
-  const receipt = await tx.wait();
-  const contract = receipt.contractAddress;
-  console.log(`deployed contract address: ${contract}`);
-
-  const verify = await signer.call({
     to: contract,
     data: data.calldata,
   });
-  console.log(`verify result: ${verify}`);
+  console.log(`verify proof tx: ${tx.hash}`);
 };
 
 main();
